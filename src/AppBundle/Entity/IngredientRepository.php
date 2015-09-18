511fa37f88f63364e7451d5e24748b008e01ec93
Noel Light-Hilary <noel@noellh.com>
2015-09-18 01:14:55 +0100

    Add convenience method getOrMake() for ingredients.

--- src/AppBundle/Entity/IngredientRepository.php
+++ src/AppBundle/Entity/IngredientRepository.php
@@ -10,4 +10,21 @@ namespace AppBundle\Entity;
  */
 class IngredientRepository extends \Doctrine\ORM\EntityRepository
 {
+    /**
+     * Gets the ingredient with the given name if any exists, or makes one if not and returns that
+     *
+     * @param string $ingredientName
+     * @return Ingredient
+     */
+    public function getOrMake($ingredientName)
+    {
+        $ingredient = $this->findOneBy(['name' => $ingredientName]);
+
+        if ($ingredient === null) {
+            $ingredient = (new Ingredient)
+                ->setName($ingredientName);
+        }
+
+        return $ingredient;
+    }
 }
